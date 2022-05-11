96cef6445174a69354fd82b6981159178127442f
Thanh Nguyen <congthanh10dt06@gmail.com>
2022-05-11 17:13:59 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Component, Fragment } from 'react';
+import './App.css';
+
+
+class AppComponent extends Component {
+  constructor() {
+
+  }
+
+  render() {
+    <Fragment>
+      <div>
+        Hello world!
+      </div>
+    </Fragment>
+  }
+}
+
+export default AppComponent
